8f44fead66fc4ee3713106c3a802e39e21d75243
hexa-hedron <hwr.bluebird@gmail.com>
2020-11-15 19:21:58 +0900

    action等追加

--- src/actions/App.js
+++ src/actions/App.js
@@ -1,27 +1,47 @@
-export const chageTaskName = (task) => {
+export const CHANGE_TASK_NAME = 'CHANGE_TASK_NAME';
+export const CHANGE_TASK_TYPE = 'CHANGE_TASK_TYPE';
+export const INITIALIZE_FORM = 'INITIALIZE_FORM';
+export const ADD_TASK = 'ADD_TASK';
+export const COMPLETE_TASK = 'COMPLETE_TASK';
+export const DELETE_TASK = 'DELETE_TASK';
+
+export const chageTaskName = (taskName) => {
     return{
-        type: 'CHANGE_TASK_NAME',
-        payload: { task }
+        type: CHANGE_TASK_NAME,
+        payload: { taskName }
     };
+}
+
+export const changeTaskType = (taskType) => {
+    return{
+        type: CHANGE_TASK_TYPE,
+        payload: { taskType }
+    }
+}
+
+export const initializeForm = () =>{
+    return{
+        type: INITIALIZE_FORM
+    }
 }
 
 export const addTask = (task) => {
     return {
-        type: 'ADD_TASK',
+        type: ADD_TASK,
         payload: { task }
     };
 }
 
 export const completeTask = (task) => {
     return{
-        type: 'COMPLETE_TASK',
+        type: COMPLETE_TASK,
         payload: { task }
     };
 }
 
 export const deleteTask = (task) => {
     return{
-        type: 'DELETE_TASK',
+        type: DELETE_TASK,
         payload: { task }
     };
 }
--- src/reducers/App.js
+++ src/reducers/App.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux'
+import { CHANGE_TASK_NAME,CHANGE_TASK_TYPE,INITIALIZE_FORM,ADD_TASK,COMPLETE_TASK,DELETE_TASK } from '../actions/App'
 
 const initialState = {
     user: {
@@ -7,20 +8,31 @@ const initialState = {
         point: 0
     },
     form: {
-        taskname: '',
+        taskName: '',
         taskType: 0
     },
     task: {
-        tasks: []
+        tasks: [[],[],[],[]]
     }
 }
 
 // フォーム上の変更を受け取る
-const formReducer = (state = initialState, action) => {
+const formReducer = (state = initialState.form, action) => {
     switch(action.type){
-        // TODO
+        case CHANGE_TASK_NAME:
+            return{
+                ...state,
+                taskName = action.taskName,
+            }
+        case CHANGE_TASK_TYPE:
+            return{
+                ...state,
+                taskType = action.taskType,
+            }
+        case INITIALIZE_FORM:
+            return initialState.form
         default:
-            return state;
+            return state
     }
 }
 
